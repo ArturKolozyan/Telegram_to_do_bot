69f156fea82dc92947d20d3dde7218d7046619d5
Artur <arturkolozyan19122006@gmail.com>
2025-12-11 09:58:08 +0300

    docker + readme

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USERNAME = os.getenv('DB_USERNAME')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOSTNAME = os.getenv('DB_HOSTNAME')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
+DB_USERNAME = os.getenv('DB_USERNAME', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'admin')
+DB_HOSTNAME = os.getenv('DB_HOSTNAME', 'postgres')
+DB_PORT = os.getenv('DB_PORT', '5432')
+DB_NAME = os.getenv('DB_NAME', 'telegram_to_do_bot')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8561202842:AAHhrQ4bmwVzkkQab5wUAOFvvESnQMcBX40')
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
 DB_URL = f'postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}'
--- main.py
+++ main.py
@@ -4,17 +4,16 @@ from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 from app.handlers import router
-# from db.db import DbOperations
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-dp.include_router(router)
+from db.db import DbOperations
 
 
 async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
     logging.basicConfig(level='INFO')
-    # await DbOperations.delete_db()
-    # await DbOperations.create_db()
+    await DbOperations.delete_db()
+    await DbOperations.create_db()
     await dp.start_polling(bot)
 
 
